bf894923e5eec2d85bad96b64f8fcd0d79f9df5b
inodesign21 <inodesign21@gmail.com>
2014-11-18 19:04:52 -0200

    thing
    accordin new and changes visual

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
 $(document).ready(function() {
     $.material.init();
 });
+
+// Accordion Toggle Icon
+function toggleChevron(e) {
+  $(e.target)
+      .prev('.panel-heading')
+      .find("i.indicator")
+      .toggleClass('glyphicon-chevron-down glyphicon-chevron-up');
+}
+$('#accordion').on('hidden.bs.collapse', toggleChevron);
+$('#accordion').on('shown.bs.collapse', toggleChevron);
